e91841f239e66f650111aef9f3819c108206ee34
PavanMahuli <67958814+PavanMahuli@users.noreply.github.com>
2024-06-06 14:06:36 -0400

    Create lambda_function.py

---
+++ lambda_function.py
@@ -0,0 +1,11 @@
+import json
+import os
+
+def lambda_handler(event, context):
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!'),
+        'pass': os.environ['passcode'],
+        'mydata': os.environ['mydata']
+    }
